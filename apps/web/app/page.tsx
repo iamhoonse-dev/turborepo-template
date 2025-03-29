9486ce52a4a3bec20a5c4e2ab29ec34f7aee2474
iamhoonse <iamhoonse@gmail.com>
2025-03-30 00:48:35 +0900

    chore(sample): 모노레포 내 다른 어플리케이션 워크스페이스에서 utils 패키지를 사용하는 샘플 코드 작성

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
+import { isEmptyString } from "@repo/utils/string";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -36,6 +37,12 @@ export default function Home() {
             Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>
+            isEmptyString : {JSON.stringify(isEmptyString("  \t    \n  "))}
+          </li>
+          <li>
+            isEmptyString : {JSON.stringify(isEmptyString("  \t  t  \n  "))}
+          </li>
         </ol>
 
         <div className={styles.ctas}>
