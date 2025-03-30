5cf0e34c9c6cf73d5664d1c248de947e74840854
iamhoonse <iamhoonse@gmail.com>
2025-03-30 18:08:04 +0900

    build: react-utils 패키지에서 빌드 시 vite-tsconfig-paths 플러그인 적용

--- packages/react-utils/vite.config.ts
+++ packages/react-utils/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dtsPlugin from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [dtsPlugin(), react()],
+  plugins: [dtsPlugin(), react(), tsConfigPaths()],
   build: {
     lib: {
       name: "react-utils",
