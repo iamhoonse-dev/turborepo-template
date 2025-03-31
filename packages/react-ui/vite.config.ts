1652dcf31edd4a715c1aa5e86386787b89fd5fb8
iamhoonse <iamhoonse@gmail.com>
2025-03-31 19:55:33 +0900

    build: react-ui 패키지 빌드 시 Vite 설정 구성 파일 작성

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import dtsPlugin from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dtsPlugin(), react()],
+  build: {
+    lib: {
+      name: "react-ui",
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+  },
+  resolve: { alias: { src: resolve(__dirname, "src/") } },
 });
