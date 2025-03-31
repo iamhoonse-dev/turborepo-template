a88559dc19a6e5eefc7b08dbc77b2aa919211f70
iamhoonse <iamhoonse@gmail.com>
2025-03-31 20:48:37 +0900

    build: react-ui 패키지에서 빌드 시 src 하위 디렉토리별 index.ts 파일을 entry 로 빌드하고 export 하도록 설정 변경

---
+++ packages/react-ui/src/base/shadcn-ui/index.ts
@@ -0,0 +1,3 @@
+/* Button 컴포넌트 */
+export { buttonVariants } from "@/base/shadcn-ui/buttonVariants";
+export { Button } from "@/base/shadcn-ui/button";
--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, type LibraryOptions } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dtsPlugin from "vite-plugin-dts";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+
+/**
+ * Generates the file name for the library based on the format and entry name.
+ *
+ * @param format - The format of the library (e.g., "es" or "cjs").
+ * @param entryName - The name of the entry point.
+ * @returns The generated file name.
+ */
+const fileName: Exclude<LibraryOptions["fileName"], string> = (
+  format,
+  entryName,
+) => {
+  switch (entryName) {
+    case "shadcn-ui":
+      return `base/shadcn-ui/index.${format}.js`;
+    case "utils":
+      return `lib/utils.${format}.js`;
+    default:
+      return `${entryName}/index.${format}.js`;
+  }
+};
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +32,12 @@ export default defineConfig({
   build: {
     lib: {
       name: "react-ui",
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: {
+        "shadcn-ui": resolve(__dirname, "src/base/shadcn-ui/index.ts"),
+        utils: resolve(__dirname, "src/lib/utils.ts"),
+      },
       formats: ["es", "cjs"],
-      fileName: (format) => `index.${format}.js`,
+      fileName,
     },
   },
   resolve: { alias: { "@": resolve(__dirname, "./src") } },
