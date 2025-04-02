b6c4a1ee9e1888cb740320d5842ab107737034eb
iamhoonse <iamhoonse@gmail.com>
2025-04-02 21:52:54 +0900

    build: storybook 앱에서 packages/react-ui 워크스페이스에 정의된 스토리를 표시할 수 있도록 연동 설정

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -10,7 +10,11 @@ function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/react-ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
