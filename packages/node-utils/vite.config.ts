9392cd7efc72b5243ba283026b0131a2629a8618
iamhoonse <iamhoonse@gmail.com>
2025-04-06 01:33:10 +0900

    build: node-utils 패키지 빌드 시 Vite 설정 구성 파일 작성

---
+++ packages/node-utils/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dtsPlugin from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    dtsPlugin({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      name: "node-utils",
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+  },
+  resolve: { alias: { src: resolve("src/") } },
+});
