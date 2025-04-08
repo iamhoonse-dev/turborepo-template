6648d4992acc95b1f888a56d48da6cf03fc07740
iamhoonse <iamhoonse@gmail.com>
2025-04-08 21:59:35 +0900

    chore(test): browser-utils 패키지에서 테스트 케이스에 있던 동적 import 문을 await 하도록 테스트 케이스를 async 함수로 변경

--- packages/browser-utils/src/dom/alertHelloWorld/index.spec.ts
+++ packages/browser-utils/src/dom/alertHelloWorld/index.spec.ts
@@ -1,13 +1,11 @@
 import { describe, test, expect, vi } from "vitest";
 
 describe("alertHelloWorld()", () => {
-  test("should alert 'Hello World'", () => {
+  test("should alert 'Hello World'", async () => {
     const alertMock = vi.spyOn(window, "alert").mockImplementation(() => {});
-    import(".").then((module) => {
-      const alertHelloWorld = module.default;
-      alertHelloWorld();
-      expect(alertMock).toHaveBeenCalledWith("Hello World");
-      alertMock.mockRestore();
-    });
+    const { default: alertHelloWorld } = await import(".");
+    alertHelloWorld();
+    expect(alertMock).toHaveBeenCalledWith("Hello World");
+    alertMock.mockRestore();
   });
 });
