710972451c6695ee1f6781970b6090129bb6f6e2
iamhoonse <iamhoonse@gmail.com>
2025-04-08 21:41:53 +0900

    chore(test): browser-utils 패키지에 @vitest/coverage-v8 사용을 위한 vite config 설정

--- packages/browser-utils/vite.config.ts
+++ packages/browser-utils/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import dtsPlugin from "vite-plugin-dts";
@@ -20,4 +21,10 @@ export default defineConfig({
     },
   },
   resolve: { alias: { src: resolve(__dirname, "src/") } },
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["json"],
+    },
+  },
 });
