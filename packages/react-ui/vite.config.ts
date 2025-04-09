87a64856144e5e055eab74333a5de653bcb1c2f4
iamhoonse <iamhoonse@gmail.com>
2025-04-09 20:51:58 +0900

    chore(test): react-ui 패키지에 @testing-library/react 사용을 위한 vitest config 추가

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -71,6 +71,7 @@ export default defineConfig({
   resolve: { alias: { "@": resolve(__dirname, "./src") } },
   test: {
     environment: "jsdom",
+    setupFiles: ["./src/tests/setup.ts"],
     coverage: {
       provider: "v8",
       reporter: ["json"],
