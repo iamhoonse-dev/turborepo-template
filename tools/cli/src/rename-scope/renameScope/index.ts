2492738cfdaeb4364ab0691f2559dd6773e3e41a
iamhoonse <iamhoonse@gmail.com>
2025-04-28 23:22:57 +0900

    feat: cli 도구에서 rename-scope 명령어 추가

---
+++ tools/cli/src/rename-scope/renameScope/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Renames the scope of a package name from @repo/ to @<name>/
+ *
+ * @param name - The new name for the package scope
+ */
+export default function renameScope(name: string) {
+  // todo: implement the renameScope function
+  console.log(`renaming to : "${name}"`);
+}
