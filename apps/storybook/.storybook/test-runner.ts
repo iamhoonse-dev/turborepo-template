d915ff232ff812c736368d93fcbefc01c0153bce
iamhoonse <iamhoonse@gmail.com>
2025-05-05 00:12:15 +0900

    test(report): storybook 앱에서 테스트 러너 실행 중 a11y 검사 결과 리포트를 junit 형식으로 저장하도록 설정 추가

--- apps/storybook/.storybook/test-runner.ts
+++ apps/storybook/.storybook/test-runner.ts
@@ -1,5 +1,6 @@
 import type { TestRunnerConfig } from "@storybook/test-runner";
-import { injectAxe, checkA11y } from "axe-playwright";
+import { getStoryContext } from "@storybook/test-runner";
+import { injectAxe, checkA11y, configureAxe } from "axe-playwright";
 
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
@@ -9,14 +10,33 @@ const config: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page) {
-    await checkA11y(page, "body", {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true,
-      },
+  async postVisit(page, context) {
+    // Get the entire context of a story, including parameters, args, argTypes, etc.
+    const storyContext = await getStoryContext(page, context);
+
+    // Apply story-level a11y rules
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
     });
+
+    // element to check
+    const element = storyContext.parameters?.a11y?.element ?? "body";
+
+    // Check a11y for current story and report results in JUnit format
+    await checkA11y(
+      page,
+      element,
+      {
+        detailedReport: true,
+      },
+      true,
+      "junit",
+      {
+        outputDirPath: "test-results",
+        outputDir: "a11y-audit",
+        reportFileName: `${storyContext.title}.xml`,
+      },
+    );
   },
 };
-
 export default config;
