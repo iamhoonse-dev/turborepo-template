cefa5abdbaaba86009cbbe5a400d99d88b22ff32
iamhoonse <iamhoonse@gmail.com>
2025-05-05 22:41:25 +0900

    ci(lint): lighthouse-ci 도구에 대해서는 @typescript-eslint/no-require-imports 룰을 적용하지 않도록 설정 추가

--- tools/lighthouse-ci/eslint.config.mjs
+++ tools/lighthouse-ci/eslint.config.mjs
@@ -1,4 +1,9 @@
 import { config } from "@repo/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...config,
+  // exclude TypeScript specific rules
+  // because we are not using TypeScript in this workspace
+  { rules: { "@typescript-eslint/no-require-imports": "off" } },
+];
