4e2c32d9386d4008ee50c417ba639c16f197e46c
iamhoonse <iamhoonse@gmail.com>
2025-05-06 03:20:44 +0900

    test: lighthouse-ci 도구에서 assert 를 사용하지 않도록 비활성화 설정

--- tools/lighthouse-ci/.lighthouserc.cjs
+++ tools/lighthouse-ci/.lighthouserc.cjs
@@ -7,28 +7,28 @@ module.exports = {
       startServerCommand: "echo 'Server will be started by docker-compose'",
       settings: { chromeFlags: "--no-sandbox" },
     },
-    assert: {
-      preset: "lighthouse:recommended",
-      assertions: {
-        /* Errors for web */
-        "bf-cache": "off",
-        "color-contrast": "off",
-        "heading-order": "off",
-        "offscreen-images": "off",
-        "unminified-javascript": "off",
-        "unused-javascript": "off",
-        "valid-source-maps": "off",
-        "errors-in-console": "off",
-        "redirects-http": "off",
-
-        /* Warnings for web */
-        interactive: "off",
-        "largest-contentful-paint": "off",
-        "legacy-javascript": "off",
-        "is-on-https": "off",
-        "render-blocking-resources": "off",
-      },
-    },
+    // assert: {
+    //   preset: "lighthouse:recommended",
+    //   assertions: {
+    //     /* Errors for web */
+    //     "bf-cache": "off",
+    //     "color-contrast": "off",
+    //     "heading-order": "off",
+    //     "offscreen-images": "off",
+    //     "unminified-javascript": "off",
+    //     "unused-javascript": "off",
+    //     "valid-source-maps": "off",
+    //     "errors-in-console": "off",
+    //     "redirects-http": "off",
+    //
+    //     /* Warnings for web */
+    //     interactive: "off",
+    //     "largest-contentful-paint": "off",
+    //     "legacy-javascript": "off",
+    //     "is-on-https": "off",
+    //     "render-blocking-resources": "off",
+    //   },
+    // },
     upload: {
       target: "temporary-public-storage",
     },
