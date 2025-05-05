31affc125195a01ed4864a392d0e278a61b26394
iamhoonse <iamhoonse@gmail.com>
2025-05-05 22:50:21 +0900

    chore(test): lighthouse-ci 도구에서 테스트 실행을 위한 설정 구성 파일 작성 및 npm 명령어 추가

---
+++ tools/lighthouse-ci/.lighthouserc.cjs
@@ -0,0 +1,33 @@
+const URLSs = require("./configs/target-urls/index.js");
+
+module.exports = {
+  ci: {
+    collect: {
+      url: URLSs,
+      startServerCommand: "echo 'Server will be started by docker-compose'",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+      assertions: {
+        /* Errors for web */
+        "bf-cache": "off",
+        "color-contrast": "off",
+        "heading-order": "off",
+        "offscreen-images": "off",
+        "unminified-javascript": "off",
+        "unused-javascript": "off",
+        "valid-source-maps": "off",
+
+        /* Warnings for web */
+        interactive: "off",
+        "largest-contentful-paint": "off",
+        "legacy-javascript": "off",
+        "": "off",
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
+// export default config;
