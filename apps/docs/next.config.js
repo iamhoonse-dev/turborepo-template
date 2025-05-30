e6270e3053314a414d0ea8f90ffb1183c9b23436
iamhoonse <iamhoonse@gmail.com>
2025-05-30 22:41:22 +0900

    build(deps): docs 앱에 nextra 통합

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,13 @@
+import nextra from "nextra";
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withNextra({
+  ...nextConfig,
+});
