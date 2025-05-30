80bdcc076e2c462177e37c8cc3f77bded43657c8
iamhoonse <iamhoonse@gmail.com>
2025-05-30 22:16:28 +0900

    refactor: docs 앱의 이름을 empty-next-app 로 변경

--- apps/empty-next-app/app/page.tsx
+++ apps/empty-next-app/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
+            Get started by editing <code>apps/empty-next-app/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
