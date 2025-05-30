8431102895b6b208b6bb17f2906d9290f6e8f05b
iamhoonse <iamhoonse@gmail.com>
2025-05-30 22:57:02 +0900

    build: docs 앱을 GitHub Pages 로 배포하기 위해 정적 페이지로 빌드하도록 설정

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -10,4 +10,15 @@ const nextConfig = {};
 
 export default withNextra({
   ...nextConfig,
+
+  // Set the output to "export" for static site generation
+  output: "export",
+
+  // set basePath to "/turborepo-template" to match the GitHub Pages deployment
+  basePath: "/turborepo-template",
+
+  // disable the default Next.js image optimization to avoid issues with GitHub Pages
+  images: {
+    unoptimized: true,
+  },
 });
