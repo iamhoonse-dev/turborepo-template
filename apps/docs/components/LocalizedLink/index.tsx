b1296dbeb92f82f05728e84a3142b9e4d919e51a
iamhoonse <iamhoonse@gmail.com>
2025-06-06 18:12:50 +0900

    feat: docs 앱에서 LocalizedLink 컴포넌트가 문자열 href 만 지원하도록 사양 변경

--- apps/docs/components/LocalizedLink/index.tsx
+++ apps/docs/components/LocalizedLink/index.tsx
@@ -3,12 +3,41 @@
 import type { FC, ReactNode, ComponentProps } from "react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import type { LocaleRouteParams } from "../../app/[locale]/layout";
+
+/**
+ * getLocalizedHref is a utility function that takes a locale and an href
+ *
+ * @param locale
+ * @param href
+ */
+function getLocalizedHref(
+  locale: LocaleRouteParams["locale"],
+  href?: Props["href"],
+) {
+  // locale root path is the base path for the locale
+  const localeRootPath = `/${locale}`;
+
+  // if no href is provided, we return the locale root path
+  if (!href) return localeRootPath;
+
+  // if href is an absolute path, we return it as is
+  if (href.startsWith("http://") || href.startsWith("https://")) {
+    return href;
+  }
+
+  // If href is already a full path, we return it as is
+  if (href.startsWith("/")) return `${localeRootPath}${href}`;
+
+  // If href is not a full path, we prepend the locale root path
+  return `${localeRootPath}/${href}`;
+}
 
 interface Props {
   // The children to render inside the link
   children: ReactNode;
   // The href can be a string or an object, similar to Next.js Link
-  href?: ComponentProps<typeof Link>["href"];
+  href?: string;
   // The target attribute for the link, defaulting to "_blank"
   target?: ComponentProps<typeof Link>["target"];
 }
@@ -23,7 +52,7 @@ interface Props {
  */
 const LocalizedLink: FC<Props> = ({ children, href, target = "_blank" }) => {
   // Use Next.js router to get the current locale from the URL parameters
-  const { locale } = useParams();
+  const { locale } = useParams<LocaleRouteParams>();
 
   // If no href is provided, we simply render the children without a link
   if (!href) {
@@ -31,7 +60,7 @@ const LocalizedLink: FC<Props> = ({ children, href, target = "_blank" }) => {
   }
 
   // If the href is a string, we can safely append the locale
-  const localizedHref = typeof href === "string" ? `/${locale}${href}` : href;
+  const localizedHref = getLocalizedHref(locale, href);
 
   return (
     <Link href={localizedHref} target={target}>
