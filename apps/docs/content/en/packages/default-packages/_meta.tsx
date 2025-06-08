1022bdceb1ba5a4dae6d891e87e24f43f68eeb25
iamhoonse <iamhoonse@gmail.com>
2025-06-08 16:40:14 +0900

    docs: docs 앱에서 각 언어별로 샘플 패키지 문서 세트 추가

---
+++ apps/docs/content/en/packages/default-packages/_meta.tsx
@@ -0,0 +1,20 @@
+export default {
+  "browser-utils": {
+    // "title": "",
+  },
+  "eslint-plugin-sample": {
+    // "title": "",
+  },
+  "http-clients": {
+    // "title": "",
+  },
+  "node-utils": {
+    // "title": "",
+  },
+  "react-ui": {
+    // "title": "",
+  },
+  "react-utils": {
+    // "title": "",
+  },
+};
