26040ae250f5870578190ff50445a5b84473deb8
iamhoonse <iamhoonse@gmail.com>
2025-06-24 23:11:38 +0900

    build: docs 앱에서 사용할 환경 변수 GITHUB_REPOSITORY 추가

--- apps/docs/env.d.ts
+++ apps/docs/env.d.ts
@@ -2,6 +2,8 @@ declare module "process" {
   global {
     namespace NodeJS {
       interface ProcessEnv {
+        // GITHUB_REPOSITORY 는 코드 링크 URL 구성에 사용되는 GitHub 저장소 이름입니다.
+        GITHUB_REPOSITORY: `${string}/${string}`;
         // GITHUB_REPO는 Giscus 에서 사용하는 GitHub 저장소 이름입니다.
         GISCUS_REPO: `${string}/${string}`;
         // GITHUB_REPO_ID는 Giscus 에서 사용하는 GitHub 저장소 ID 입니다.
