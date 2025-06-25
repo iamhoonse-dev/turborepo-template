92fc40d2ac9878d3496160942a8b89748ec548c2
iamhoonse <iamhoonse@gmail.com>
2025-06-25 22:01:35 +0900

    feat: react-utils 패키지에 useLocalStorageListener 훅 추가

--- packages/react-utils/src/hooks/index.ts
+++ packages/react-utils/src/hooks/index.ts
@@ -1,3 +1,4 @@
 /* v8 ignore start */
 export { default as useInterval } from "./useInterval";
+export { default as useLocalStorageChangeListener } from "./useLocalStorageChangeListener";
 /* v8 ignore stop */
---
+++ packages/react-utils/src/hooks/useLocalStorageChangeListener/index.ts
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+
+/**
+ * Custom hook to register a listener for localStorage changes.
+ *
+ * @param targetKey - The key in localStorage to listen for changes.
+ * @param onLocalStorageChange - Optional callback function that will be called with the new value when the localStorage changes.
+ *
+ * @example
+ * ```tsx
+ * const TARGET_KEY = ""myKey";
+ *
+ * useLocalStorageChangeListener(TARGET_KEY, (newValue) => {
+ *   console.log(`"${TARGET_KEY}" changed to : `, newValue);
+ * });
+ * ```
+ */
+export default function useLocalStorageChangeListener(
+  targetKey: string,
+  onLocalStorageChange?: (value: StorageEvent["newValue"]) => void,
+): void {
+  useEffect(
+    function registerLocalStorageChangeListener() {
+      /**
+       * Function to handle localStorage changes.
+       *
+       * @param event - The StorageEvent object containing information about the change.
+       */
+      function handleLocalStorageChange(event: StorageEvent) {
+        // Check if the event is for the specific key we are interested in
+        const { key, newValue } = event;
+        if (key !== targetKey) return;
+
+        // Call the provided callback function with the new value
+        onLocalStorageChange?.(newValue);
+      }
+
+      // Register the localStorage change listener
+      window.addEventListener("storage", handleLocalStorageChange);
+
+      /**
+       * Cleanup function to remove the localStorage change listener.
+       */
+      return function cleanupLocalStorageChangeListener() {
+        window.removeEventListener("storage", handleLocalStorageChange);
+      };
+    },
+    [targetKey, onLocalStorageChange],
+  );
+}
